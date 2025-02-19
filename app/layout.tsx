6baddb0ce9579f7bcb21e15d54be7dfb2dd34a7c
Amin Zibaei <Rector.dev@gmail.com>
2025-02-19 18:58:45 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono, Outfit, Young_Serif, Inter,Manrope } from "next/font
 import "./globals.css";
 
 const manrope= Manrope({subsets:["latin"],variable:"--font-manrope"}) 
-const inter = Inter({variable:"--font-inter"});
+const inter = Inter({variable:"--font-inter",subsets:["latin"]});
 const youngSerif = Young_Serif({ variable: "--font-young-serif", subsets: ["latin"], weight: ["400"] });
 const outfit = Outfit({ variable: "--font-outfit" });
 const geistSans = Geist({
